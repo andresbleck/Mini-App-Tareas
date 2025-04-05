0825c71201310209e446fa1eaf54aab47a56a149
andresbleck <andres.bleckwedel2@gmail.com>
2025-04-04 23:59:08 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
-const listaDeTareas = [];
+const listaDeTareas = []; // Array para simular la base de datos de tareas
 
+//Funcion para Guardar una tarea
 function guardarEnBaseDeDatos(tarea) {
   listaDeTareas.push(tarea);
   return `Tarea guardada: ${tarea}`;
 }
-
+//exporta la funcion
 module.exports = {
   guardarEnBaseDeDatos,
 };
